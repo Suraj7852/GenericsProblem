bd264635cadea809af5079cb2c4f5d8a8929e5f1
Suraj7852 <surajpj7852@gmail.com>
2019-11-28 18:10:34 +0530

    ADDED: created method for printing maximum using Generic Method

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -10,6 +10,14 @@ public class MaximumNumber<X,Y,Z extends Comparable>  {
     }
 
     public static <E extends  Comparable> E findMaximum(E i, E i1, E i2) {
-        return (i.compareTo(i1)>0?i:i1.compareTo(i2)>0?i1:i2);
+        E max;
+        max = (i.compareTo(i1)>0?i:i1.compareTo(i2)>0?i1:i2);
+        printMax(max);
+        return max;
     }
+
+    private static <E extends  Comparable> void printMax(E max) {
+        System.out.println(max);
+    }
+
 }
