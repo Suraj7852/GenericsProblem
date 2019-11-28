1a16bfb710f38c917bc89686dde189e83dbfc658
Suraj7852 <surajpj7852@gmail.com>
2019-11-28 17:48:00 +0530

    REFACTOR: One Generic Method

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,13 +1,11 @@
-public class MaximumNumber {
-    public Integer findMaxNumber(Integer i, Integer i1, Integer i2) {
-        return i.compareTo(i1)>0?i:i1.compareTo(i2)>0?i1:i2;
+public class MaximumNumber<E extends Comparable>  {
+    E intNumber;
+
+    public MaximumNumber(E intNumber) {
+        this.intNumber = intNumber;
     }
 
-    public Double findMaxDoubleNumber(Double v, Double v1, Double v2) {
-        return v.compareTo(v1)>0?v:v1.compareTo(v2)>0?v1:v2;
-    }
-
-    public String findMaxString(String anish, String suraj, String zafar) {
-        return anish.compareTo(suraj)>0?anish:suraj.compareTo(zafar)>0?suraj:zafar;
+    public static <E extends  Comparable> E findMaximum(E i, E i1, E i2) {
+        return (i.compareTo(i1)>0?i:i1.compareTo(i2)>0?i1:i2);
     }
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -4,40 +4,37 @@ import org.junit.Test;
 public class MaximumNumberTest {
     @Test
     public void given3NumberWillReturnGreaterNumberAmong3() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        Integer maxNumber = maximumNumber.findMaxNumber(6,8,9);
+        Integer maxNumber = MaximumNumber.findMaximum(6,8,9);
         Assert.assertEquals(new Integer(9),maxNumber);
 
-        Integer maxNumber1 = maximumNumber.findMaxNumber(6,10,2);
+        Integer maxNumber1 = MaximumNumber.findMaximum(6,10,2);
         Assert.assertEquals(new Integer(10),maxNumber1);
 
-        Integer maxNumber2 = maximumNumber.findMaxNumber(12,8,9);
-        Assert.assertEquals(new Integer(12),maxNumber2);
-    }
+        Integer maxNumber2 = (Integer) MaximumNumber.findMaximum(6,8,19);
+        Assert.assertEquals(Integer.valueOf(19),maxNumber2);
+   }
 
     @Test
     public void given3FloatNumberWillReturnGreaterNumberAmong3() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        Double doubleNumber = maximumNumber.findMaxDoubleNumber(1.1, 2.2, 3.2);
+        Double doubleNumber = MaximumNumber.findMaximum(1.1, 2.2, 3.2);
         Assert.assertEquals(new Double(3.2),doubleNumber);
 
-        Double doubleNumber1 = maximumNumber.findMaxDoubleNumber(3.2, 2.2, 1.1);
+        Double doubleNumber1 = MaximumNumber.findMaximum(3.2, 2.2, 1.1);
         Assert.assertEquals(new Double(3.2),doubleNumber1);
 
-        Double doubleNumber2 = maximumNumber.findMaxDoubleNumber(1.1, 3.2, 3.2);
+        Double doubleNumber2 = MaximumNumber.findMaximum(1.1, 3.2, 3.2);
         Assert.assertEquals(new Double(3.2),doubleNumber2);
     }
 
     @Test
     public void given3StringWillReturnGreaterStringAmong3() {
-        MaximumNumber maximumNumber = new MaximumNumber();
-        String maxString = maximumNumber.findMaxString("anish", "suraj", "zafar");
+        String maxString = MaximumNumber.findMaximum("akshay", "suraj", "zafar");
         Assert.assertEquals("zafar",maxString);
 
-        String maxString1 = maximumNumber.findMaxString("zafar", "suraj", "anish");
+        String maxString1 = MaximumNumber.findMaximum("zafar", "suraj", "akshay");
         Assert.assertEquals("zafar",maxString1);
 
-        String maxString2 = maximumNumber.findMaxString("anish", "zafar", "suraj");
+        String maxString2 = MaximumNumber.findMaximum("akshay", "zafar", "suraj");
         Assert.assertEquals("zafar",maxString2);
     }
 }
